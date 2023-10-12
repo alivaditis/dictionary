0425f5191da82924f1f02743c0a74f4811097819
Alec Livaditis <aleclivaditis@gmail.com>
2023-10-12 17:14:02 -0400

    add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect,  } from 'react';
 import { getWord } from './api';
 import { SingleWord } from './types';
-import './styles/App.scss';
+import './styles/App.css';
 import Meaning from './components/Meaning';
 import Searchbar from './components/Searchbar';
 
 function App() {
   const [word, setWord] = useState<SingleWord | undefined>()
   const [query, setQuery] = useState('nice')
+  const [isDark, setIsDark] = useState(false)
 
   const submitSearch = (e: React.FormEvent) => {
     e.preventDefault()
@@ -23,20 +24,20 @@ function App() {
   const meanings = word?.meanings.map(meaning => <Meaning meaning={meaning}/>)
 
   return (
-    <div className="App">
+    <div className ='app' data-theme={isDark ? 'dark' : 'light'}>
       <Searchbar query={query} setQuery={setQuery} submitSearch={submitSearch}/>
       <div className='word-container'>
         <div className='word-column'>
           <h1>
             {word?.word}
           </h1>
-          <p>
+          <p className='phonetic'>
             {word?.phonetics[0].text && word?.phonetics[0].text}
           </p>
         </div>
         {word && meanings}
         <p>Source</p>
-        <a href={word?.sourceUrls[0]}>{word?.sourceUrls[0]}</a>
+        <a className='source' href={word?.sourceUrls[0]}>{word?.sourceUrls[0]}</a>
       </div>
     </div>
   );
